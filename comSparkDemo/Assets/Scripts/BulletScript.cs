863ec866f924e3329801fe9617159da1ac86f9fc
Justin Morgan <Rythious@gmail.com>
2017-09-26 21:40:22 -0400

    Added bullet damage.

---
+++ comSparkDemo/Assets/Scripts/BulletScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletScript : MonoBehaviour {
+
+	void OnTriggerEnter2D (Collider2D other) {
+        other.gameObject.SendMessage("BlowUp", SendMessageOptions.DontRequireReceiver);
+
+        Destroy(gameObject);
+    }
+}
